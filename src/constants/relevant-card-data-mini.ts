097af9f38615d09681ff08b46bace4f6606a22a8
Ivan Kotov <ivankotov12345@gmail.com>
2025-04-18 15:06:48 +0300

    fix(relevant-section): fix id data type

--- src/constants/relevant-card-data-mini.ts
+++ src/constants/relevant-card-data-mini.ts
@@ -6,17 +6,17 @@ import pot from '~/assets/svg/pot.svg';
 
 export const VEGAN_RELEVANT_CARD_DATA_MINI = [
     {
-        id: 1,
+        id: '1',
         image: pan,
         title: 'Стейк для вегетарианцев',
     },
     {
-        id: 2,
+        id: '2',
         image: pan,
         title: 'Котлеты из гречки и фасоли',
     },
     {
-        id: 3,
+        id: '3',
         image: pot,
         title: 'Сырный суп с лапшой и брокколи',
     },
@@ -24,17 +24,17 @@ export const VEGAN_RELEVANT_CARD_DATA_MINI = [
 
 export const DESSERTS_RELEVANT_CARD_DATA_MINI = [
     {
-        id: 1,
+        id: '1',
         image: kid,
         title: 'Домашние сырные палочки',
     },
     {
-        id: 2,
+        id: '2',
         image: international,
         title: 'Панкейки',
     },
     {
-        id: 3,
+        id: '3',
         image: leave,
         title: 'Воздушное банановое печенье на сковороде',
     },
