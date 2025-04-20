c2661144ab5adeb2c1bdecace1cc05a4d550df08
Ivan Kotov <ivankotov12345@gmail.com>
2025-04-20 17:55:09 +0300

    feat(ingredients-table): add increment/decrement portions logic

--- src/components/ingredients-table/ingredients-table.tsx
+++ src/components/ingredients-table/ingredients-table.tsx
@@ -13,6 +13,7 @@ import {
     Thead,
     Tr,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import { IngredientType } from '~/types/recepie';
 
@@ -21,38 +22,60 @@ type IngredientsTableProps = {
     portions?: number;
 };
 
-export const IngredientsTable: React.FC<IngredientsTableProps> = ({ ingredients, portions }) => (
-    <Table variant='striped' colorScheme='blackAlpha'>
-        <Thead>
-            <Tr>
-                <Th>
-                    <Text variant='limeUppercase'>Ингредиенты</Text>
-                </Th>
-                <Th px={0}>
-                    <HStack justifyContent='end'>
-                        <Text variant='limeUppercase'>Порций</Text>
+export const IngredientsTable: React.FC<IngredientsTableProps> = ({ ingredients, portions }) => {
+    const [inputPortions, setInputPortions] = useState(portions || 1);
+    const [currentIngredients, setCurrentIngredients] = useState(ingredients);
 
-                        <NumberInput value={portions || 0} w={24}>
-                            <NumberInputField />
-                            <NumberInputStepper>
-                                <NumberIncrementStepper />
-                                <NumberDecrementStepper />
-                            </NumberInputStepper>
-                        </NumberInput>
-                    </HStack>
-                </Th>
-            </Tr>
-        </Thead>
+    const getOnePortionSize = (count: string, portions: number = 1) => +count / portions;
 
-        <Tbody>
-            {ingredients.map(({ title, count, measureUnit }) => (
-                <Tr key={title}>
-                    <Td>{title}</Td>
-                    <Td display='flex' justifyContent='end'>
-                        {+count > 0 && count} {measureUnit}
-                    </Td>
+    const onPortionsChange = (_: string, value: number) => {
+        const updated = currentIngredients.map((ingredient) => ({
+            ...ingredient,
+            count: `${getOnePortionSize(ingredient.count, inputPortions) * value}`,
+        }));
+
+        setCurrentIngredients(updated);
+        setInputPortions(value);
+    };
+
+    return (
+        <Table variant='striped' colorScheme='blackAlpha'>
+            <Thead>
+                <Tr>
+                    <Th>
+                        <Text variant='limeUppercase'>Ингредиенты</Text>
+                    </Th>
+                    <Th px={0}>
+                        <HStack justifyContent='end'>
+                            <Text variant='limeUppercase'>Порций</Text>
+
+                            <NumberInput
+                                value={inputPortions}
+                                min={1}
+                                onChange={onPortionsChange}
+                                w={24}
+                            >
+                                <NumberInputField />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </HStack>
+                    </Th>
                 </Tr>
-            ))}
-        </Tbody>
-    </Table>
-);
+            </Thead>
+
+            <Tbody>
+                {currentIngredients.map(({ title, count, measureUnit }) => (
+                    <Tr key={title}>
+                        <Td>{title}</Td>
+                        <Td display='flex' justifyContent='end'>
+                            {+count > 0 && count} {measureUnit}
+                        </Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
+    );
+};
